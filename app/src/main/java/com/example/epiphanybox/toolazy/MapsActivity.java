6da6cbf078d3531f8e2650082d830a34b1eb6a43
SalasMi <salasmi@sonoma.edu>
2016-04-27 21:26:19 -0700

    maps displays sydney

--- app/src/main/java/com/example/epiphanybox/toolazy/MapsActivity.java
+++ app/src/main/java/com/example/epiphanybox/toolazy/MapsActivity.java
@@ -20,8 +20,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+                .findFragmentById(R.id.Map);
         mapFragment.getMapAsync(this);
+
     }
 
 
